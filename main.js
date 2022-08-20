51ec63f2144d4fb90d2660f08df45a4995f3bda7
Sabimael Anaguaya <sabimael.anaguaya@gmail.com>
2022-08-20 16:42:49 -0400

    finished header section

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 document.querySelector('.menu-open-close').addEventListener("click", openMenuBar);
 
 
+
 function openMenuBar (){
     let menuBar = document.querySelector('.header-container');
-    menuBar.classList.toggle("close")
+    let line1 = document.querySelector('.line1');
+    let line2 = document.querySelector('.line2');
+    let line3 = document.querySelector('.line3');
+    
+    menuBar.classList.toggle("close");
+    line1.classList.toggle("line1__activated");
+    line2.classList.toggle("line2__activated");
+    line3.classList.toggle("line3__activated");
+
 }
